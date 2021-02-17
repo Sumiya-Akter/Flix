2ca6b98f7ca7a5f0de4f0e2cb0be276af1eeb438
Sumiya Akter <sakter9012@gmail.com>
2021-02-17 15:15:31 -0500

    boop

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sumiya Akter on 2/17/21.
+//  Copyright © 2021 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
